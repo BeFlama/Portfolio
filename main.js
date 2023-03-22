eb14acc2cad079d2e69d7253bf8b31cd48756c99
BeFlama <florencia.n.benitez@hotmail.com>
2023-03-22 01:10:38 -0300

    agregue datos del portfolio

---
+++ main.js
@@ -0,0 +1,19 @@
+const typed = new Typed('.typed', {
+    strings : [
+        '<i class="moveTitle">web designer</i>',
+        '<i class="moveTitle">UX/UI designer</i>',
+        '<i class="moveTitle">front end developer</i>',
+        '<i class="moveTitle">onlyFans member</i>'
+    ],
+    typeSpeed: 75,
+    startDelay: 300,
+    backSpeed: 50,
+    smartBackspace: true,
+    shuffle: false,
+    backDelay: 1000,
+    loop: true,
+    loopCpunt: false,
+    showCursor: true,
+    cursorChar: '|',
+    contentType: 'html'
+})
