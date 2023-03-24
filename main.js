00b4d9de01942d9f7442957307e0cf2e2b191c86
BeFlama <florencia.n.benitez@hotmail.com>
2023-03-24 17:21:10 -0300

    90%

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const typed = new Typed('.typed', {
         '<i class="moveTitle">web designer</i>',
         '<i class="moveTitle">UX/UI designer</i>',
         '<i class="moveTitle">front end developer</i>',
-        '<i class="moveTitle">onlyFans member</i>'
+        '<i class="moveTitle">a nurse too</i>'
     ],
     typeSpeed: 75,
     startDelay: 300,
